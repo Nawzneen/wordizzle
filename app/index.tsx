5f74ffd78de9c8e578148535c518f0d9e08397c3
nawzneen <earworm90@gmail.com>
2024-08-01 13:25:30 +0300

    init

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { View, Text } from "react-native";
+import React from "react";
+import "../styles/styles.css";
+
+const index = () => {
+  return (
+    <View className="flex-1 mt-16">
+      <View className="bg-blue-600">
+        <Text className="text-white font-bold ">APP</Text>
+      </View>
+    </View>
+  );
+};
+
+export default index;
